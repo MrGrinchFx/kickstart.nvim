92a86e45c0db3c17d7192bfca2f206bab68df4e1
Kevin <2003kevinle@gmail.com>
2025-08-26 03:45:42 -0700

    added keymap documentation

--- lua/MrGrinch/config/keymaps.lua
+++ lua/MrGrinch/config/keymaps.lua
@@ -8,22 +8,22 @@ vim.g.have_nerd_font = true
 
 -- Yank to system clipboard
 -- require the options and keymaps, and autocmds
-vim.keymap.set('n', '<leader>y', '"+y')
-vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>y', '"+y', { desc = 'yanks to system clipboard from normal mode' })
+vim.keymap.set('v', '<leader>y', '"+y', { desc = 'yanks to system clipboard from visual mode' })
 
 -- Paste without losing content
-vim.keymap.set('x', '<leader>p', '"_dP')
+vim.keymap.set('x', '<leader>p', '"_dP', { desc = 'paste without losing register content' })
 
 -- move highlighted section
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'move highlighted text up' })
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'move highlighted text down' })
 
 -- J doesnt move cursor
-vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Shift + J does not move the cursor' })
 
 -- half page down or up and centers
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'moves half a screen up' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'moves half a screen up' })
 vim.keymap.set('n', '<leader>pv', '<CMD>Oil<CR>', { desc = 'Open Oil' })
 -- make Ctrl-c be the same as Escape
 vim.keymap.set('i', 'C-c', '<Esc>')
