0823daa360244887326beb961e3cb539fb1512e9
Kevin <2003kevinle@gmail.com>
2025-09-05 02:14:49 -0700

    solarized-osaka

--- lua/MrGrinch/plugins/colors.lua
+++ lua/MrGrinch/plugins/colors.lua
@@ -1,24 +1,51 @@
+--return { -- You can easily change to a different colorscheme.
+--  -- Change the name of the colorscheme plugin below, and then
+--  -- change the command in the config to whatever the name of that colorscheme is.
+--  --
+--  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+--  'folke/tokyonight.nvim',
+--  priority = 1000, -- Make sure to load this before all the other start plugins.
+--  config = function()
+--    ---@diagnostic disable-next-line: missing-fields
+--    require('tokyonight').setup {
+--      transparent = true,
+--      styles = {
+--        comments = { italic = false }, -- Disable italics in comments
+--        sidebars = 'transparent',
+--        floats = 'transparent',
+--      },
+--    }
+--
+--    -- Load the colorscheme here.
+--    -- Like many other themes, this one has different styles, and you could load
+--    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+--    vim.cmd.colorscheme 'solarized-osaka'
+--  end,
+--}
+
 return { -- You can easily change to a different colorscheme.
   -- Change the name of the colorscheme plugin below, and then
   -- change the command in the config to whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'folke/tokyonight.nvim',
+  'craftzdog/solarized-osaka.nvim',
+  lazy = false,
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
     ---@diagnostic disable-next-line: missing-fields
-    require('tokyonight').setup {
+    require('solarized-osaka').setup {
       transparent = true,
       styles = {
-        comments = { italic = false }, -- Disable italics in comments
-        sidebars = 'transparent',
-        floats = 'transparent',
+        comments = { italic = true }, -- Disable italics in comments
+        keyword = { italic = true },
+        sidebars = 'dark',
+        floats = 'dark',
       },
     }
 
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-storm'
+    vim.cmd.colorscheme 'solarized-osaka'
   end,
 }
