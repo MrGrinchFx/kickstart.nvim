edab0077351b493d74c62d45d56737477af29288
Kevin Le <2003kevinle@gmail.com>
2025-09-18 16:06:32 -0700

    added solarized osaka theme

--- lua/MrGrinch/plugins/colors.lua
+++ lua/MrGrinch/plugins/colors.lua
@@ -23,29 +23,22 @@
 --  end,
 --}
 
-return { -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+return {
   'craftzdog/solarized-osaka.nvim',
   lazy = false,
-  priority = 1000, -- Make sure to load this before all the other start plugins.
+  priority = 1000,
   config = function()
-    ---@diagnostic disable-next-line: missing-fields
+    -- This is where you configure and load the colorscheme.
     require('solarized-osaka').setup {
+      -- The setup function is equivalent to the opts table.
       transparent = true,
-      styles = {
-        comments = { italic = true }, -- Disable italics in comments
-        keyword = { italic = true },
-        sidebars = 'dark',
-        floats = 'dark',
-      },
+      -- You can also add other options here, for example:
+      -- styles = {
+      --   comments = { 'italic' },
+      -- },
     }
 
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    -- Now, activate the colorscheme.
     vim.cmd.colorscheme 'solarized-osaka'
   end,
 }
