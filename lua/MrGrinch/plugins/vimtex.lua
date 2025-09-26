d9c7c16f8846fd9a348e8e45c417b0f6099625c9
Kevin <2003kevinle@gmail.com>
2025-09-26 15:29:59 -0700

    added latex support

--- lua/MrGrinch/plugins/vimtex.lua
+++ lua/MrGrinch/plugins/vimtex.lua
@@ -5,5 +5,13 @@ return {
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = 'zathura'
+    vim.g.vimtex_quickfix_mode = 0
+    vim.opt.conceallevel = 1
+    vim.g.tex_conceal = 'abdmg'
+    vim.g.vimtex_view_forward_search_on_start = false
+    vim.g.maplocalleader = ' '
+    local opts = { noremap = true, silent = true }
+
+    vim.api.nvim_set_keymap('n', '<leader>ls', ':TSBufDisable highlight<CR>', opts)
   end,
 }
